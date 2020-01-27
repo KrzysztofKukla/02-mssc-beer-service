e16ace3ff05163c1a519b310292894436bacb184
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-01-27 21:44:15 +0100

    57 Beer Jpa Entity, closes #5

---
+++ src/main/java/pl/kukla/krzys/msscbeerservice/domain/Beer.java
@@ -0,0 +1,59 @@
+package pl.kukla.krzys.msscbeerservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * @author Krzysztof Kukla
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID") //Hibernate automatically will generate UUID settings for us
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+    private BigDecimal price;
+
+    @Version //this gives us optimistic locking
+    private Long version;
+
+    //Hibernate is implementation of JPA
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp modifiedDate;
+
+}
