2b0ece940beb4f308f61cfe08652c0e16a29452d
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-02-05 23:09:12 +0100

    114 @JsonCreator, closes #14

--- src/main/java/pl/kukla/krzys/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/pl/kukla/krzys/msscbeerservice/web/model/BeerPagedList.java
@@ -1,6 +1,10 @@
 package pl.kukla.krzys.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -8,8 +12,29 @@ import java.util.List;
 /**
  * @author Krzysztof Kukla
  */
+//Page Object is a standard inside of Spring
 public class BeerPagedList extends PageImpl<BeerDto> {
-    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+
+    //this allows Jackson to communicate with another Spring service which has a page property and then bind to BeerPagedList
+    //it allows to binding those all fields
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(
+        @JsonProperty("content") List<BeerDto> content,
+        @JsonProperty("number") int number,
+        @JsonProperty("size") int size,
+        @JsonProperty("totalElements") Long totalElements,
+        @JsonProperty("pageable") JsonNode pageable,
+        @JsonProperty("last") boolean last,
+        @JsonProperty("totalPages") int totalPages,
+        @JsonProperty("sort") JsonNode sort,
+        @JsonProperty("first") boolean first,
+        @JsonProperty("numberOfElements") int numberOfElements
+    ) {
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
+    public BeerPagedList(List<BeerDto> content,
+                         Pageable pageable, long total) {
         super(content, pageable, total);
     }
 
